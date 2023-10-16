75323acee641fa77c01b2c4230321285e7fee35f
PredatoryEagle <computer.m2020@gmail.com>
2023-10-16 20:39:31 +0300

    add container widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -30,39 +27,108 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: const Color(0xffC0D1DD),
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text("مـرحــبا",
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color(0xff0A4D68),
+              fontFamily: 'ElMessiri-VariableFont_wght'
+            )),
+          backgroundColor: const Color(0xffC0D1DD),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                height: 228,
+                width: 310,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("زيارة مرتقبة",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'ElMessiri-VariableFont_wght',
+                          fontSize: 16.0,
+                        ),
+                        ),
+                        Icon(
+                          Icons.comment
+                        )
+                      ],
+                    ),
+                    Divider(
+                      color: Colors.black,
+                      thickness: 1.5,
+                      height: 25.5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('IMAGE'),
+                        SizedBox(
+                          width: 25.0,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("قم بزيارة",
+                            style: TextStyle(
+                              fontFamily: 'ElMessiri-VariableFont_wght',
+                              color: Colors.black,
+                              fontSize: 10.0
+                            ),
+                            ),
+                            Text('عمي احمد',
+                            style: TextStyle(
+                              fontFamily: 'ElMessiri-VariableFont_wght',
+                              color: Colors.black,
+                              fontSize: 20.0
+                            ),), 
+                            Text('في اقرب وقت',
+                            style: TextStyle(
+                              fontFamily: 'ElMessiri-VariableFont_wght',
+                              color: Colors.black,
+                              fontSize: 10.0
+                            ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    IconButton.filled(onPressed:(){}, icon: 
+                    Icon(Icons.call,
+                    color: Color(0xff0A4D68),
+                    ),
+                    ),
+                    ],
+                ),
+              ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        )
       ),
     );
   }
